49a2444d071c833416ae731e20f08f2201674c9c
TAINÁ ESTEFANI MARTINS <taina_e_martins@estudante.sesisenai.org.br>
2025-03-31 21:40:35 -0300

    feat: implementação de métodos buscarUsuario, atualizarUsuario e removerUsuario na classe GerenciadorUsuario

--- ControleEpi/src/GerenciadorUsuario.java
+++ ControleEpi/src/GerenciadorUsuario.java
@@ -30,4 +30,49 @@ public class GerenciadorUsuario {
     public void listarNomesUsuarios() {
         usuarios.forEach(usuario -> System.out.println((usuarios.indexOf(usuario) + 1) + ": " + usuario.getNome()));
     }
+
+    public Usuario buscarUsuario() {
+        try {
+            listarUsuarios();
+            System.out.println("Digite o índice do usuário: ");
+
+            Usuario usuario = usuarios.get(scanner.nextInt());
+            scanner.nextLine();
+            if (usuario == null) throw new IllegalArgumentException("Erro ao encontrar usuário. Tente novamente!");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            buscarUsuario();
+        }
+        return null;
+    }
+
+    public void atualizarUsuario() {
+        try {
+            Usuario usuario = buscarUsuario();
+
+            System.out.println("1. Atualizar nome\n2. Atualizar e-mail");
+            int opcao = scanner.nextInt();
+            if (opcao == 1) {
+                System.out.println("Novo nome: ");
+                usuario.setNome(scanner.nextLine());
+            } else if (opcao == 2) {
+                System.out.println("Novo e-mail: ");
+                usuario.setEmail(scanner.nextLine());
+            } else {
+                throw new IllegalArgumentException("Opção inválida. Tente novamente!");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            atualizarUsuario();
+        }
+    }
+
+    public void removerUsuario() {
+        try {
+            Usuario usuario = buscarUsuario();
+            usuarios.remove(usuario);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
