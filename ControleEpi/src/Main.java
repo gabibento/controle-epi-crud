d4cb39bcfc77e8a6a0d302d66a2e011487420c00
Gabriella <gabriella.mabento@gmail.com>
2025-04-03 09:25:28 -0300

    fix: ajustes estruturas de repetição na Main

--- ControleEpi/src/Main.java
+++ ControleEpi/src/Main.java
@@ -13,6 +13,7 @@ public class Main {
         GerenciadorEmprestimo gerenciadorEmprestimo = new GerenciadorEmprestimo();
         GerenciadorDevolucao gerenciadorDevolucao = new GerenciadorDevolucao();
         int opcao;
+        int opcao2 = 0;
 
         do{
             System.out.println("1. CRUD de Usuários\n" +
@@ -25,49 +26,50 @@ public class Main {
 
             if(opcao == 0) break;
 
-            System.out.println("1)Cadastrar\n" +
-                    "2)Listar\n" +
-                    "3)Atualizar\n" +
-                    "4)Remover");
+            do{
+                System.out.println("1. Cadastrar\n" +
+                        "2. Listar\n" +
+                        "3. Atualizar\n" +
+                        "4. Remover\n" +
+                        "5. Voltar");
+                System.out.print("Escolha uma opção: ");
+                opcao2 = scanner.nextInt();
 
-            System.out.print("Escolha uma opção: ");
-            int opcao2 = scanner.nextInt();
-
-
-            switch (opcao){
-                case 1 -> {
-                    switch (opcao2){
-                        case 1: gerenciadorUsuario.cadastrarUsuario();
-                        case 2: gerenciadorUsuario.listarUsuarios();
-                        case 3: gerenciadorUsuario.atualizarUsuario();
-                        case 4: gerenciadorUsuario.removerUsuario();
+                switch (opcao){
+                    case 1 -> {
+                        switch (opcao2){
+                            case 1 -> gerenciadorUsuario.cadastrarUsuario();
+                            case 2 -> gerenciadorUsuario.listarUsuarios();
+                            case 3 -> gerenciadorUsuario.atualizarUsuario();
+                            case 4 -> gerenciadorUsuario.removerUsuario();
+                        }
+                    }
+                    case 2 -> {
+                        switch (opcao2){
+                            case 1 -> gerenciadorEpi.cadastrarEpi();
+                            case 2 -> gerenciadorEpi.listarEpis();
+                            case 3 -> gerenciadorEpi.atualizarEpi();
+                            case 4 -> gerenciadorEpi.removerEpi();
+                        }
+                    }
+                    case 3 -> {
+                        switch (opcao2){
+                            case 1 -> gerenciadorEmprestimo.criarEmprestimo();
+                            case 2 -> gerenciadorEmprestimo.listarEmprestimos();
+                            case 3 -> gerenciadorEmprestimo.atualizarEmprestimo();
+                            case 4 -> gerenciadorEmprestimo.removerEmprestimo();
+                        }
+                    }
+                    case 4 -> {
+                        switch (opcao2){
+                            case 1 -> gerenciadorDevolucao.criarDevolucao();
+                            case 2 -> gerenciadorDevolucao.listarDevolucoes();
+                            case 3 -> gerenciadorDevolucao.atualizarDevolucao();
+                            case 4 -> gerenciadorDevolucao.removerDevolucao();
+                        }
                     }
                 }
-                case 2 -> {
-                    switch (opcao2){
-                        case 1: gerenciadorEpi.cadastrarEpi();
-                        case 2: gerenciadorEpi.listarEpis();
-                        case 3: gerenciadorEpi.atualizarEpi();
-                        case 4: gerenciadorEpi.removerEpi();
-                    }
-                }
-                case 3 -> {
-                    switch (opcao2){
-                        case 1: gerenciadorEmprestimo.criarEmprestimo();
-                        case 2: gerenciadorEmprestimo.listarEmprestimos();
-                        case 3: gerenciadorEmprestimo.atualizarEmprestimo();
-                        case 4: gerenciadorEmprestimo.removerEmprestimo();
-                    }
-                }
-                case 4 -> {
-                    switch (opcao2){
-                        case 1: gerenciadorDevolucao.criarDevolucao();
-                        case 2: gerenciadorDevolucao.listarDevolucoes();
-                        case 3: gerenciadorDevolucao.atualizarDevolucao();
-                        case 4: gerenciadorDevolucao.removerDevolucao();
-                    }
-                }
-            }
+            }while(opcao2 != 5);
         }while (opcao != 0);
     }
 }
