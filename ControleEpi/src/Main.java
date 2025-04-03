9bb90e114d3e686b901785cc7a1eeb87c8d2d11c
Gabriella <gabriella.mabento@gmail.com>
2025-04-03 10:31:50 -0300

    fix: separação de processamento de menus por métodos

--- ControleEpi/src/Main.java
+++ ControleEpi/src/Main.java
@@ -3,73 +3,106 @@ import gerenciadores.GerenciadorEmprestimo;
 import gerenciadores.GerenciadorEpi;
 import gerenciadores.GerenciadorUsuario;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+     static final Scanner scanner = new Scanner(System.in);
+     static final GerenciadorEpi gerenciadorEpi = new GerenciadorEpi();
+     static final GerenciadorUsuario gerenciadorUsuario = new GerenciadorUsuario();
+     static final GerenciadorEmprestimo gerenciadorEmprestimo = new GerenciadorEmprestimo();
+     static final GerenciadorDevolucao gerenciadorDevolucao = new GerenciadorDevolucao();
+
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        GerenciadorEpi gerenciadorEpi = new GerenciadorEpi();
-        GerenciadorUsuario gerenciadorUsuario = new GerenciadorUsuario();
-        GerenciadorEmprestimo gerenciadorEmprestimo = new GerenciadorEmprestimo();
-        GerenciadorDevolucao gerenciadorDevolucao = new GerenciadorDevolucao();
-        int opcao;
-        int opcao2 = 0;
+        try {
+            processarMenu();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida! Tente novamente");
+            processarMenu();
+        } finally {
+            scanner.close();
+        }
+    }
+    private static void processarMenu(){
+        do {
+            int opcao = escolherMenuPrincipal();
+            if (opcao == 0) break;
 
-        do{
-            System.out.println("1. CRUD de Usuários\n" +
-                    "2. CRUD de EPIs\n" +
-                    "3. CRUD de Empréstimos\n" +
-                    "4. CRUD de Devoluções\n" +
-                    "0. Sair");
-            System.out.print("Escolha uma opção: ");
-            opcao = scanner.nextInt();
+            processarOpcaoPrincipal(opcao);
+        } while (true);
+    }
+    private static int escolherMenuPrincipal() {
+        System.out.println("\n1. CRUD de Usuários\n" +
+                "2. CRUD de EPIs\n" +
+                "3. CRUD de Empréstimos\n" +
+                "4. CRUD de Devoluções\n" +
+                "0. Sair");
+        System.out.print("Escolha uma opção: ");
+        return scanner.nextInt();
+    }
 
-            if(opcao == 0) break;
+    private static int escolherCRUD() {
+        System.out.println("\n1. Cadastrar\n" +
+                "2. Listar\n" +
+                "3. Atualizar\n" +
+                "4. Remover\n" +
+                "5. Voltar");
+        System.out.print("Escolha uma opção: ");
+        return scanner.nextInt();
+    }
 
-            do{
-                System.out.println("1. Cadastrar\n" +
-                        "2. Listar\n" +
-                        "3. Atualizar\n" +
-                        "4. Remover\n" +
-                        "5. Voltar");
-                System.out.print("Escolha uma opção: ");
-                opcao2 = scanner.nextInt();
+    private static void processarOpcaoPrincipal(int opcao) {
+        do {
+            int opcaoCRUD = escolherCRUD();
+            if (opcaoCRUD == 5) break;
 
-                switch (opcao){
-                    case 1 -> {
-                        switch (opcao2){
-                            case 1 -> gerenciadorUsuario.cadastrarUsuario();
-                            case 2 -> gerenciadorUsuario.listarUsuarios();
-                            case 3 -> gerenciadorUsuario.atualizarUsuario();
-                            case 4 -> gerenciadorUsuario.removerUsuario();
-                        }
-                    }
-                    case 2 -> {
-                        switch (opcao2){
-                            case 1 -> gerenciadorEpi.cadastrarEpi();
-                            case 2 -> gerenciadorEpi.listarEpis();
-                            case 3 -> gerenciadorEpi.atualizarEpi();
-                            case 4 -> gerenciadorEpi.removerEpi();
-                        }
-                    }
-                    case 3 -> {
-                        switch (opcao2){
-                            case 1 -> gerenciadorEmprestimo.criarEmprestimo();
-                            case 2 -> gerenciadorEmprestimo.listarEmprestimos();
-                            case 3 -> gerenciadorEmprestimo.atualizarEmprestimo();
-                            case 4 -> gerenciadorEmprestimo.removerEmprestimo();
-                        }
-                    }
-                    case 4 -> {
-                        switch (opcao2){
-                            case 1 -> gerenciadorDevolucao.criarDevolucao();
-                            case 2 -> gerenciadorDevolucao.listarDevolucoes();
-                            case 3 -> gerenciadorDevolucao.atualizarDevolucao();
-                            case 4 -> gerenciadorDevolucao.removerDevolucao();
-                        }
-                    }
-                }
-            }while(opcao2 != 5);
-        }while (opcao != 0);
+            switch (opcao) {
+                case 1 -> processarCRUDUsuario(opcaoCRUD);
+                case 2 -> processarCRUDEpi(opcaoCRUD);
+                case 3 -> processarCRUDEmprestimo(opcaoCRUD);
+                case 4 -> processarCRUDDevolucao(opcaoCRUD);
+                default -> System.out.println("Opção inválida.");
+            }
+        } while (true);
+    }
+    private static void processarCRUDUsuario(int opcao) {
+        switch (opcao) {
+            case 1 -> gerenciadorUsuario.cadastrarUsuario();
+            case 2 -> gerenciadorUsuario.listarUsuarios();
+            case 3 -> gerenciadorUsuario.atualizarUsuario();
+            case 4 -> gerenciadorUsuario.removerUsuario();
+            default -> System.out.println("Opção inválida.");
+        }
+    }
+
+    private static void processarCRUDEpi(int opcao) {
+        switch (opcao) {
+            case 1 -> gerenciadorEpi.cadastrarEpi();
+            case 2 -> gerenciadorEpi.listarEpis();
+            case 3 -> gerenciadorEpi.atualizarEpi();
+            case 4 -> gerenciadorEpi.removerEpi();
+            default -> System.out.println("Opção inválida.");
+        }
+    }
+
+    private static void processarCRUDEmprestimo(int opcao) {
+        switch (opcao) {
+            case 1 -> gerenciadorEmprestimo.criarEmprestimo();
+            case 2 -> gerenciadorEmprestimo.listarEmprestimos();
+            case 3 -> gerenciadorEmprestimo.atualizarEmprestimo();
+            case 4 -> gerenciadorEmprestimo.removerEmprestimo();
+            default -> System.out.println("Opção inválida.");
+        }
+    }
+
+    private static void processarCRUDDevolucao(int opcao) {
+        switch (opcao) {
+            case 1 -> gerenciadorDevolucao.criarDevolucao();
+            case 2 -> gerenciadorDevolucao.listarDevolucoes();
+            case 3 -> gerenciadorDevolucao.atualizarDevolucao();
+            case 4 -> gerenciadorDevolucao.removerDevolucao();
+            default -> System.out.println("Opção inválida.");
+        }
     }
 }
